bfa3a5dcd3774d0c33ca5d46af717996d98709b8
Fabari <agfabariagbora@gmail.com>
2025-02-28 19:51:45 +0100

    Update server listen URL to use production endpoint

--- server.js
+++ server.js
@@ -200,4 +200,4 @@ app.post('/investments', async (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on https://personal-finance-dashboard-uu8u.onrender.com/${PORT}`));
